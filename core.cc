6cff15fa8d44195e4248c304ec0a9af8c81661a3
Rafal Lalik <rafal.lalik@uj.edu.pl>
2018-01-11 09:23:30 +0100

    New histograms, debug output

--- core.cc
+++ core.cc
@@ -31,10 +31,6 @@ Int_t core(HLoop * loop, const AnaParameters & anapars)
     //////////////////////////////////////////////////////////////////////////////
 
     loop->printCategories();    // print all categories found in input + status
-    //     loop->printChain();            // print all files in the chain
-    //     loop->Print();
-
-    //     HEventHeader * header = loop->getEventHeader();
 
     HCategory * fCatGeantKine = nullptr;
     fCatGeantKine = HCategoryManager::getCategory(catGeantKine, kTRUE, "catGeantKine");
@@ -63,6 +59,22 @@ Int_t core(HLoop * loop, const AnaParameters & anapars)
     TH1I * h_hit_rpc = new TH1I("h_hit_rpc", "Tracks mult;multiplicity", 10, 0, 10);
     TH2I * h_hit_s01_str = new TH2I("h_hit_s01_str", "Tracks mult;multiplicity", 3, 0, 3, 20, 0, 20);
 
+    TH1 * h_hit_mult_hades_p = new TH1I("h_hit_mult_hades_p", "Protons in Hades mult;multiplicity", 10, 0, 10);
+    TH1 * h_hit_mult_hades_pip = new TH1I("h_hit_mult_hades_pip", "pi-plus in Hades mult;multiplicity", 10, 0, 10);
+    TH1 * h_hit_mult_hades_pim = new TH1I("h_hit_mult_hades_pim", "pi-minus in Hades mult;multiplicity", 10, 0, 10);
+    TH1 * h_hit_mult_hades_Kp = new TH1I("h_hit_mult_hades_Kp", "K-plus in Hades mult;multiplicity", 10, 0, 10);
+    TH1 * h_hit_mult_hades_Km = new TH1I("h_hit_mult_hades_Km", "K-minus in Hades mult;multiplicity", 10, 0, 10);
+    TH1 * h_hit_mult_hades = new TH1I("h_hit_mult_hades", "Charged in Hades mult;multiplicity", 10, 0, 10);
+
+    TH1 * h_hit_mult_fwdet_p = new TH1I("h_hit_mult_fwdet_p", "Protons in Hades mult;multiplicity", 10, 0, 10);
+    TH1 * h_hit_mult_fwdet_pip = new TH1I("h_hit_mult_fwdet_pip", "pi-plus in Hades mult;multiplicity", 10, 0, 10);
+    TH1 * h_hit_mult_fwdet_pim = new TH1I("h_hit_mult_fwdet_pim", "pi-minus in Hades mult;multiplicity", 10, 0, 10);
+    TH1 * h_hit_mult_fwdet_Kp = new TH1I("h_hit_mult_fwdet_Kp", "K-plus in Hades mult;multiplicity", 10, 0, 10);
+    TH1 * h_hit_mult_fwdet_Km = new TH1I("h_hit_mult_fwdet_Km", "K-minus in Hades mult;multiplicity", 10, 0, 10);
+    TH1 * h_hit_mult_fwdet = new TH1I("h_hit_mult_fwdet", "Charged in Hades mult;multiplicity", 10, 0, 10);
+
+    TH2 * h_hit_mult_hades_fwdet = new TH2I("h_hit_mult_hades_fwdet", "Charged in Hades/FwDet mult;multiplicity;multiplicity", 10, 0, 10, 10, 0, 10);
+
     for (Int_t i = 0; i < entries; i++)                    // event loop
     {
         /*Int_t nbytes =*/  loop->nextEvent(i);         // get next event. categories will be cleared before
@@ -71,12 +83,29 @@ Int_t core(HLoop * loop, const AnaParameters & anapars)
         int tracks_num = fCatGeantKine->getEntries();
         h_tr_mult->Fill(tracks_num);
 
-        Int_t m0, m1, m2, m3, s0, s1, str, rpc;
-        for (int i = 0; i < tracks_num; ++i)
+        Int_t cnt_h_p = 0;
+        Int_t cnt_h_pip = 0;
+        Int_t cnt_h_pim = 0;
+        Int_t cnt_h_Kp = 0;
+        Int_t cnt_h_Km = 0;
+
+        Int_t cnt_f_p = 0;
+        Int_t cnt_f_pip = 0;
+        Int_t cnt_f_pim = 0;
+        Int_t cnt_f_Kp = 0;
+        Int_t cnt_f_Km = 0;
+
+        Int_t cnt_h = 0;
+        Int_t cnt_f = 0;
+
+        printf("Event %d\n", i);
+        for (int j = 0; j < tracks_num; ++j)
         {
-            HGeantKine * pKine = (HGeantKine *)fCatGeantKine->getObject(i);
+            HGeantKine * pKine = (HGeantKine *)fCatGeantKine->getObject(j);
             if (!pKine)
                 continue;
+
+            Int_t m0 = 0, m1 = 0, m2 = 0, m3 = 0, s0 = 0, s1 = 0, str = 0, rpc = 0;
 
             pKine->getNHitsDecayBit(m0, m1, m2, m3, s0, s1);
             pKine->getNHitsFWDecayBit(str, rpc);
@@ -86,7 +115,53 @@ Int_t core(HLoop * loop, const AnaParameters & anapars)
             h_hit_str->Fill(str);
             h_hit_rpc->Fill(rpc);
             h_hit_s01_str->Fill(s0+s1, str);
+
+            if (s0 or s1)
+            {
+                Int_t pid = pKine->getID();
+                switch (pid)
+                {
+                    case 8: ++ cnt_h_pip; ++cnt_h; break;       // pip
+                    case 9: ++ cnt_h_pim; ++cnt_h; break;       // pim
+                    case 11: ++ cnt_h_Kp; ++cnt_h; break;       // Kp
+                    case 12: ++ cnt_h_Km; ++cnt_h; break;       // Km
+                    case 14: ++ cnt_h_p; ++cnt_h; break;        // p
+                }
+            }
+
+            if (str and rpc)
+            {
+                Int_t pid = pKine->getID();
+                switch (pid)
+                {
+                    case 8: ++ cnt_f_pip; ++cnt_f; break;       // pip
+                    case 9: ++ cnt_f_pim; ++cnt_f; break;       // pim
+                    case 11: ++ cnt_f_Kp; ++cnt_f; break;       // Kp
+                    case 12: ++ cnt_f_Km; ++cnt_f; break;       // Km
+                    case 14: ++ cnt_f_p; ++cnt_f; break;        // p
+                }
+            }
+
+            printf("  [%03d/%0d] pid=%2d parent=%d  s0=%d s1=%d  f=%d  rpc=%d\n", j, tracks_num, pKine->getID(), pKine->getParentTrack()-1, s0, s1, str, rpc);
         }
+
+        h_hit_mult_hades_p->Fill(cnt_h_p);
+        h_hit_mult_hades_pip->Fill(cnt_h_pip);
+        h_hit_mult_hades_pim->Fill(cnt_h_pim);
+        h_hit_mult_hades_Kp->Fill(cnt_h_Kp);
+        h_hit_mult_hades_Km->Fill(cnt_h_Km);
+        h_hit_mult_hades->Fill(cnt_h);
+
+        h_hit_mult_fwdet_p->Fill(cnt_f_p);
+        h_hit_mult_fwdet_pip->Fill(cnt_f_pip);
+        h_hit_mult_fwdet_pim->Fill(cnt_f_pim);
+        h_hit_mult_fwdet_Kp->Fill(cnt_f_Kp);
+        h_hit_mult_fwdet_Km->Fill(cnt_f_Km);
+        h_hit_mult_fwdet->Fill(cnt_f);
+
+        h_hit_mult_hades_fwdet->Fill(cnt_h, cnt_f);
+
+        printf("\n");
     } // end eventloop
 
     h_tr_mult->Write();
@@ -96,6 +171,22 @@ Int_t core(HLoop * loop, const AnaParameters & anapars)
     h_hit_str->Write();
     h_hit_rpc->Write();
     h_hit_s01_str->Write();
+
+    h_hit_mult_hades_p->Write();
+    h_hit_mult_hades_pip->Write();
+    h_hit_mult_hades_pim->Write();
+    h_hit_mult_hades_Kp->Write();
+    h_hit_mult_hades_Km->Write();
+    h_hit_mult_hades->Write();
+
+    h_hit_mult_fwdet_p->Write();
+    h_hit_mult_fwdet_pip->Write();
+    h_hit_mult_fwdet_pim->Write();
+    h_hit_mult_fwdet_Kp->Write();
+    h_hit_mult_fwdet_Km->Write();
+    h_hit_mult_fwdet->Write();
+
+    h_hit_mult_hades_fwdet->Write();
 
     output_file->Close();
     cout << "writing root tree done" << endl;
