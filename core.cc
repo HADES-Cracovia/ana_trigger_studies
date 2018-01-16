35825bf1248155bbb2e3676335daa12783769eba
Joanna Kubos <joanna.kubos@ifj.edu.pl>
2018-01-16 15:38:20 +0100

    allTracks

--- core.cc
+++ core.cc
@@ -25,7 +25,8 @@ Int_t core(HLoop * loop, const AnaParameters & anapars)
     }
 
     ifstream ifs;
-    std::vector<GoodTrack> goodTracks;
+    std::vector<GoodTrack> goodTracks;  //pattertn
+    
     if(anapars.config.Length()>0){
       ifs.open(anapars.config.Data(), std::fstream::in);
       if(!ifs.is_open()){
@@ -50,14 +51,17 @@ Int_t core(HLoop * loop, const AnaParameters & anapars)
 
 	int num = arr -> GetEntries();
 	for(int i = 0; i < num; i++){
-	  TObjString * s = (TObjString *)arr->At(i);
-	  int a = s->GetString().Atoi();
+	  TObjString * s = (TObjString *)arr -> At(i);
+	  int a = s -> GetString().Atoi();
 	  gd.search_path.push_back(a);
 	}
 	goodTracks.push_back(gd);
       }
     }
 
+    int n_found, n_found_hf, n_req, n_nreq, n_all;
+    float n_nra, n_fa, h_pim, f_pim, h_p, f_p;
+    
     TStopwatch timer;
     timer.Reset();
     timer.Start();
@@ -111,30 +115,71 @@ Int_t core(HLoop * loop, const AnaParameters & anapars)
 
     TH2 * h_hit_mult_hades_fwdet = new TH2I("h_hit_mult_hades_fwdet", "Charged in Hades/FwDet mult;multiplicity;multiplicity", 10, 0, 10, 10, 0, 10);
 
+    //only good/accepted events
+    TH1I * h_tr_mult_acc = new TH1I("h_tr_mult", "Tracks mult;multiplicity", 1000, 0, 1000);
+    TH1I * h_hit_s0_acc = new TH1I("h_hit_s0", "Tracks mult;multiplicity", 2, 0, 2);
+    TH1I * h_hit_s1_acc = new TH1I("h_hit_s1", "Tracks mult;multiplicity", 2, 0, 2);
+    TH1I * h_hit_s01_acc = new TH1I("h_hit_s01", "Tracks mult;multiplicity", 3, 0, 3);
+    TH1I * h_hit_str_acc = new TH1I("h_hit_str", "Tracks mult;multiplicity", 20, 0, 20);
+    TH1I * h_hit_rpc_acc = new TH1I("h_hit_rpc", "Tracks mult;multiplicity", 10, 0, 10);
+    TH2I * h_hit_s01_str_acc = new TH2I("h_hit_s01_str", "Tracks mult;multiplicity", 3, 0, 3, 20, 0, 20);
+
+    TH1 * h_hit_mult_hades_p_acc = new TH1I("h_hit_mult_hades_p", "Protons in Hades mult;multiplicity", 10, 0, 10);
+    TH1 * h_hit_mult_hades_pip_acc = new TH1I("h_hit_mult_hades_pip", "pi-plus in Hades mult;multiplicity", 10, 0, 10);
+    TH1 * h_hit_mult_hades_pim_acc = new TH1I("h_hit_mult_hades_pim", "pi-minus in Hades mult;multiplicity", 10, 0, 10);
+    TH1 * h_hit_mult_hades_Kp_acc = new TH1I("h_hit_mult_hades_Kp", "K-plus in Hades mult;multiplicity", 10, 0, 10);
+    TH1 * h_hit_mult_hades_Km_acc = new TH1I("h_hit_mult_hades_Km", "K-minus in Hades mult;multiplicity", 10, 0, 10);
+    TH1 * h_hit_mult_hades_acc = new TH1I("h_hit_mult_hades", "Charged in Hades mult;multiplicity", 10, 0, 10);
+
+    TH1 * h_hit_mult_fwdet_p_acc = new TH1I("h_hit_mult_fwdet_p", "Protons in Hades mult;multiplicity", 10, 0, 10);
+    TH1 * h_hit_mult_fwdet_pip_acc = new TH1I("h_hit_mult_fwdet_pip", "pi-plus in Hades mult;multiplicity", 10, 0, 10);
+    TH1 * h_hit_mult_fwdet_pim_acc = new TH1I("h_hit_mult_fwdet_pim", "pi-minus in Hades mult;multiplicity", 10, 0, 10);
+    TH1 * h_hit_mult_fwdet_Kp_acc = new TH1I("h_hit_mult_fwdet_Kp", "K-plus in Hades mult;multiplicity", 10, 0, 10);
+    TH1 * h_hit_mult_fwdet_Km_acc = new TH1I("h_hit_mult_fwdet_Km", "K-minus in Hades mult;multiplicity", 10, 0, 10);
+    TH1 * h_hit_mult_fwdet_acc = new TH1I("h_hit_mult_fwdet", "Charged in Hades mult;multiplicity", 10, 0, 10);
+
+    TH2 * h_hit_mult_hades_fwdet_acc = new TH2I("h_hit_mult_hades_fwdet", "Charged in Hades/FwDet mult;multiplicity;multiplicity", 10, 0, 10, 10, 0, 10);
+
+    
     for (Int_t i = 0; i < entries; i++)                    // event loop
     {
-        /*Int_t nbytes =*/  loop->nextEvent(i);         // get next event. categories will be cleared before
+        /*Int_t nbytes =*/  loop -> nextEvent(i);         // get next event. categories will be cleared before
         //cout << fCatGeantFwDet->getEntries() << endl;
 
-        int tracks_num = fCatGeantKine->getEntries();
-        h_tr_mult->Fill(tracks_num);
+        int tracks_num = fCatGeantKine -> getEntries();
+	n_all = tracks_num;
+        h_tr_mult -> Fill(tracks_num);
 
-        Int_t cnt_h_p = 0;
-        Int_t cnt_h_pip = 0;
-        Int_t cnt_h_pim = 0;
-        Int_t cnt_h_Kp = 0;
-        Int_t cnt_h_Km = 0;
+        Int_t cnt_h_p = 0, cnt_h_p_acc = 0;
+        Int_t cnt_h_pip = 0, cnt_h_pip_acc = 0;
+        Int_t cnt_h_pim = 0, cnt_h_pim_acc = 0;
+        Int_t cnt_h_Kp = 0, cnt_h_Kp_acc = 0;
+        Int_t cnt_h_Km = 0, cnt_h_Km_acc = 0;
 
-        Int_t cnt_f_p = 0;
-        Int_t cnt_f_pip = 0;
-        Int_t cnt_f_pim = 0;
-        Int_t cnt_f_Kp = 0;
-        Int_t cnt_f_Km = 0;
+        Int_t cnt_f_p = 0, cnt_f_p_acc = 0;
+        Int_t cnt_f_pip = 0, cnt_f_pip_acc = 0;
+        Int_t cnt_f_pim = 0, cnt_f_pim_acc = 0;
+        Int_t cnt_f_Kp = 0, cnt_f_Kp_acc = 0;
+        Int_t cnt_f_Km = 0, cnt_f_Km_acc = 0;
 
-        Int_t cnt_h = 0;
-        Int_t cnt_f = 0;
+        Int_t cnt_h = 0, cnt_h_acc = 0;
+        Int_t cnt_f = 0, cnt_f_acc = 0;
+	
+	h_pim = 0;
+	f_pim = 0;
+	h_p = 0;
+	f_p = 0;
+
+	n_found = 0;
+	n_found_hf = 0;
+	n_req = 0;
+	n_nreq = 0;
+	n_nra = 0;
+	n_fa = 0;
 
 	std::vector<TrackInfo> trackInf;
+	std::vector<GoodTrack> allTracks;
+	
 	const int gdnum = goodTracks.size();
 
 	for (int j = 0; j < gdnum; ++j)
@@ -143,66 +188,120 @@ Int_t core(HLoop * loop, const AnaParameters & anapars)
         printf("Event %d\n", i);
         for (int j = 0; j < tracks_num; ++j)
         {
-            HGeantKine * pKine = (HGeantKine *)fCatGeantKine->getObject(j);
+            HGeantKine * pKine = (HGeantKine *)fCatGeantKine -> getObject(j);
             if (!pKine)
                 continue;
+
+	    int check_nr = 0;
+
+	    if(pKine -> getID() == 1)   //no gamma
+	      continue;
 	    
 	    TrackInfo ti;
-	    ti.pid = pKine->getID();
+	    ti.pid = pKine -> getID();
 	    ti.track_id = j;
-	    ti.parent_id = pKine->getParentTrack()-1;
+	    ti.parent_id = pKine -> getParentTrack()-1;
 	    trackInf.push_back(ti);
 	    
-	    if(pKine->getID() == 1)
-	      continue;
-
+	    GoodTrack at;
+	    at.reset();
+	    at.pid = pKine -> getID();
+	    at.track_id = j;
+	    at.req = false;
+	        
 	    for(int k = 0; k < gdnum; k++){
-	      GoodTrack gd = goodTracks[k];
-	      //gd.print(k);
-	      if(gd.found) continue;
-	      if(gd.pid != ti.pid) continue;
+	      GoodTrack gt = goodTracks[k];
+	      gt.track_id = j;
+	      if(gt.found) continue;
+	      if(gt.pid != ti.pid) continue;   //search all p,pi-,K+
 	      TrackInfo t1 = ti;
-	      for(int l = 0; l < gd.search_path.size(); l++){
-		printf("%d    %d\n", t1.parent_id, gd.search_path[l]);
-		if(t1.parent_id == gd.search_path[l]){
+	      for(int l = 0; l < gt.search_path.size(); l++){
+		printf("%d    %d\n", t1.parent_id, gt.search_path[l]);
+		if(t1.parent_id == gt.search_path[l]){
 		  if(t1.parent_id == -1){
-		    gd.found = true;
-		    gd.track_id = j;
-		    goodTracks[k]= gd;
+		    gt.found = true;
+		    at.found = true;
+		    at.req = gt.req;
+		    goodTracks[k] = gt;
 		  }
 		  else{
 		    t1 = trackInf[t1.parent_id];
 		  }
 		}
 		else if (t1.parent_id == -1)
-		{
 		  break;
-		}
-		else
-		{
-		  if (trackInf[t1.parent_id].pid == gd.search_path[l])
+		else{
+		  if (trackInf[t1.parent_id].pid == gt.search_path[l])
 		    t1 = trackInf[t1.parent_id];
 		  else
 		    break;
 		}
 	      }
-	      if (gd.found) break;
+	      if (gt.found)
+		break;
 	    }
+
+	    allTracks.push_back(at);
+	    
+	    //printf("j=%d: allTracks: pid=%d, track_id=%d, req=%d, found=%d\n", j, allTracks.back().pid, allTracks.back().track_id, allTracks.back().req, allTracks.back().found);
+	    // printf("at: pid=%d, track_id=%d, req=%d, found=%d\n", at.pid, at.track_id, at.req, at.found);
 	    
             Int_t m0 = 0, m1 = 0, m2 = 0, m3 = 0, s0 = 0, s1 = 0, str = 0, rpc = 0;
 
-            pKine->getNHitsDecayBit(m0, m1, m2, m3, s0, s1);
+	    pKine->getNHitsDecayBit(m0, m1, m2, m3, s0, s1);
             pKine->getNHitsFWDecayBit(str, rpc);
-            h_hit_s0->Fill(s0);
-            h_hit_s1->Fill(s1);
-            h_hit_s01->Fill(s0+s1);
-            h_hit_str->Fill(str);
-            h_hit_rpc->Fill(rpc);
-            h_hit_s01_str->Fill(s0+s1, str);
+
+	    //fill histos for good events
+	    if (allTracks.back().found){
+	      h_hit_s0_acc -> Fill(s0);
+	      h_hit_s1_acc -> Fill(s1);
+	      h_hit_s01_acc -> Fill(s0+s1);
+	      h_hit_str_acc -> Fill(str);
+	      h_hit_rpc_acc -> Fill(rpc);
+	      h_hit_s01_str_acc -> Fill(s0+s1, str);
+	      h_tr_mult_acc -> Fill(tracks_num);
+
+	      if (s0 or s1)
+		{
+		  Int_t pid = pKine -> getID();
+		  switch (pid)
+		    {
+                    case 8: ++ cnt_h_pip_acc; ++cnt_h_acc; break;       // pip
+                    case 9: ++ cnt_h_pim_acc; ++cnt_h_acc; break;       // pim
+                    case 11: ++ cnt_h_Kp_acc; ++cnt_h_acc; break;       // Kp
+                    case 12: ++ cnt_h_Km_acc; ++cnt_h_acc; break;       // Km
+                    case 14: ++ cnt_h_p_acc; ++cnt_h_acc; break;        // p
+		    }
+		}
+
+	      if (str and rpc)
+		{
+		  Int_t pid = pKine -> getID();
+		  switch (pid)
+		    {
+                    case 8: ++ cnt_f_pip_acc; ++cnt_f_acc; break;       // pip
+                    case 9: ++ cnt_f_pim_acc; ++cnt_f_acc; break;       // pim
+                    case 11: ++ cnt_f_Kp_acc; ++cnt_f_acc; break;       // Kp
+                    case 12: ++ cnt_f_Km_acc; ++cnt_f_acc; break;       // Km
+                    case 14: ++ cnt_f_p_acc; ++cnt_f_acc; break;        // p
+		    }
+		}
+	      if (allTracks.back().req) n_req++;
+	      if (!allTracks.back().req) n_nreq++;
+	      n_found++;
+	      }
+
+	    //fill histos for all events
+	    h_hit_s0 -> Fill(s0);
+            h_hit_s1 -> Fill(s1);
+            h_hit_s01 -> Fill(s0+s1);
+            h_hit_str -> Fill(str);
+            h_hit_rpc -> Fill(rpc);
+            h_hit_s01_str -> Fill(s0+s1, str);
 
             if (s0 or s1)
             {
-                Int_t pid = pKine->getID();
+                Int_t pid = pKine -> getID();
                 switch (pid)
                 {
                     case 8: ++ cnt_h_pip; ++cnt_h; break;       // pip
@@ -215,7 +314,7 @@ Int_t core(HLoop * loop, const AnaParameters & anapars)
 
             if (str and rpc)
             {
-                Int_t pid = pKine->getID();
+                Int_t pid = pKine -> getID();
                 switch (pid)
                 {
                     case 8: ++ cnt_f_pip; ++cnt_f; break;       // pip
@@ -226,27 +325,71 @@ Int_t core(HLoop * loop, const AnaParameters & anapars)
                 }
             }
 
-            printf("  [%03d/%0d] pid=%2d parent=%d  s0=%d s1=%d  f=%d  rpc=%d\n", j, tracks_num, pKine->getID(), pKine->getParentTrack()-1, s0, s1, str, rpc);
+	    printf("  [%03d/%0d] pid=%2d parent=%d  s0=%d s1=%d  f=%d  rpc=%d found=%d\n", j, tracks_num, pKine->getID(), pKine->getParentTrack()-1, s0, s1, str, rpc, allTracks.back().found);
         }
 
-        h_hit_mult_hades_p->Fill(cnt_h_p);
-        h_hit_mult_hades_pip->Fill(cnt_h_pip);
-        h_hit_mult_hades_pim->Fill(cnt_h_pim);
-        h_hit_mult_hades_Kp->Fill(cnt_h_Kp);
-        h_hit_mult_hades_Km->Fill(cnt_h_Km);
-        h_hit_mult_hades->Fill(cnt_h);
+	//fill histos for good events
+	h_hit_mult_hades_p_acc -> Fill(cnt_h_p_acc);
+	h_hit_mult_hades_pip_acc -> Fill(cnt_h_pip_acc);
+	h_hit_mult_hades_pim_acc -> Fill(cnt_h_pim_acc);
+	h_hit_mult_hades_Kp_acc -> Fill(cnt_h_Kp_acc);
+	h_hit_mult_hades_Km_acc -> Fill(cnt_h_Km_acc);
+	h_hit_mult_hades_acc -> Fill(cnt_h_acc);
 
-        h_hit_mult_fwdet_p->Fill(cnt_f_p);
-        h_hit_mult_fwdet_pip->Fill(cnt_f_pip);
-        h_hit_mult_fwdet_pim->Fill(cnt_f_pim);
-        h_hit_mult_fwdet_Kp->Fill(cnt_f_Kp);
-        h_hit_mult_fwdet_Km->Fill(cnt_f_Km);
-        h_hit_mult_fwdet->Fill(cnt_f);
+	h_hit_mult_fwdet_p_acc -> Fill(cnt_f_p_acc);
+	h_hit_mult_fwdet_pip_acc -> Fill(cnt_f_pip_acc);
+	h_hit_mult_fwdet_pim_acc -> Fill(cnt_f_pim_acc);
+	h_hit_mult_fwdet_Kp_acc -> Fill(cnt_f_Kp_acc);
+	h_hit_mult_fwdet_Km_acc -> Fill(cnt_f_Km_acc);
+	h_hit_mult_fwdet_acc -> Fill(cnt_f_acc);
 
-        h_hit_mult_hades_fwdet->Fill(cnt_h, cnt_f);
+	h_hit_mult_hades_fwdet_acc -> Fill(cnt_h_acc, cnt_f_acc);
+       
+
+       	//foll histots for all events
+        h_hit_mult_hades_p -> Fill(cnt_h_p);
+        h_hit_mult_hades_pip -> Fill(cnt_h_pip);
+        h_hit_mult_hades_pim -> Fill(cnt_h_pim);
+        h_hit_mult_hades_Kp -> Fill(cnt_h_Kp);
+        h_hit_mult_hades_Km -> Fill(cnt_h_Km);
+        h_hit_mult_hades -> Fill(cnt_h);
+
+        h_hit_mult_fwdet_p -> Fill(cnt_f_p);
+        h_hit_mult_fwdet_pip -> Fill(cnt_f_pip);
+        h_hit_mult_fwdet_pim -> Fill(cnt_f_pim);
+        h_hit_mult_fwdet_Kp -> Fill(cnt_f_Kp);
+        h_hit_mult_fwdet_Km -> Fill(cnt_f_Km);
+        h_hit_mult_fwdet -> Fill(cnt_f);
+
+        h_hit_mult_hades_fwdet -> Fill(cnt_h, cnt_f);
 
         printf("\n");
 
+	n_found_hf =  cnt_h_acc + cnt_f_acc;
+	n_fa = n_found/n_all;
+	n_nra = n_nreq/n_all;
+	printf("n_found=%d, n_found_hf=%d, n_req=%d, n_nreq=%d, n_all=%d,\n n_found/n_all=%f, n_nreq/n_all=%f\n", n_found, n_found_hf, n_req, n_nreq, n_all, n_fa, n_nra);
+
+	if(cnt_h_pim!=0){
+	  h_pim = cnt_h_pim_acc/cnt_h_pim;
+	  printf("h_pi_good=%d, h_pi_all=%d, h_pi=%f\n", cnt_h_pim_acc, cnt_h_pim, h_pim);
+	}else printf("h_pi_good=%d, h_pi_all=%d\n", cnt_h_pim_acc, cnt_h_pim);
+	
+	if(cnt_f_pim!=0){
+	  f_pim = cnt_f_pim_acc/cnt_f_pim;
+	  printf("f_pi_good=%d, f_pi_all=%d, f_pi=%f\n", cnt_f_pim_acc, cnt_f_pim, f_pim);
+	}else printf("f_pi_good=%d, f_pi_all=%d\n", cnt_f_pim_acc, cnt_f_pim);
+
+	if(cnt_h_p!=0){
+	  h_p = cnt_h_p_acc/cnt_h_p;
+	  printf("h_p_good=%d, h_p_all=%d, h_p=%f\n", cnt_h_p_acc, cnt_h_p, h_p);
+	}else printf("h_p_good=%d, h_p_all=%d\n", cnt_h_p_acc, cnt_h_p);
+
+	if(cnt_f_p!=0){
+	  f_p = cnt_f_p_acc/cnt_f_p;
+	  printf("f_p_good=%d, f_p_all=%d, f_p=%f\n", cnt_f_p_acc, cnt_f_p, f_p);
+	}else printf("f_p_good=%d, f_p_all=%d\n", cnt_f_p_acc, cnt_f_p);
+	
 	for(int j = 0; j < gdnum; j++){
 	  GoodTrack gd = goodTracks[j];
 	  gd.print(j);
@@ -278,7 +421,32 @@ Int_t core(HLoop * loop, const AnaParameters & anapars)
 
     h_hit_mult_hades_fwdet->Write();
 
-    output_file->Close();
+    h_tr_mult_acc -> Write();
+    h_hit_s0_acc -> Write();
+    h_hit_s1_acc -> Write();
+    h_hit_s01_acc -> Write();
+    h_hit_str_acc -> Write();
+    h_hit_rpc_acc -> Write();
+    h_hit_s01_str_acc -> Write();
+
+    h_hit_mult_hades_p_acc -> Write();
+    h_hit_mult_hades_pip_acc -> Write();
+    h_hit_mult_hades_pim_acc -> Write();
+    h_hit_mult_hades_Kp_acc -> Write();
+    h_hit_mult_hades_Km_acc -> Write();
+    h_hit_mult_hades_acc -> Write();
+
+    h_hit_mult_fwdet_p_acc -> Write();
+    h_hit_mult_fwdet_pip_acc -> Write();
+    h_hit_mult_fwdet_pim_acc -> Write();
+    h_hit_mult_fwdet_Kp_acc -> Write();
+    h_hit_mult_fwdet_Km_acc -> Write();
+    h_hit_mult_fwdet_acc -> Write();
+
+    h_hit_mult_hades_fwdet_acc -> Write();
+    
+
+    output_file -> Close();
     cout << "writing root tree done" << endl;
 
     timer.Stop();
